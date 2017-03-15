96ac8299c64360f7da871728dc789404f3e950c4
Sid498 <sidpawar619@gmail.com>
2017-03-15 20:15:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sid/assignment_21/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.example.sid.assignment_21;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelloWorld extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_world);
+    }
+}
